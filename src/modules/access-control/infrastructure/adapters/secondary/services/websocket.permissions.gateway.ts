dfcbdac1923aa8d640259d2f51bc221cbf921ba2
MaxLyonel <leorexx100@gmail.com>
2025-10-20 00:04:45 -0400

    feat: implementacion de websocker para actualizacion en tiempo real

---
+++ src/modules/access-control/infrastructure/adapters/secondary/services/websocket.permissions.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class PermissionsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  private readonly logger = new Logger(PermissionsGateway.name);
+
+  afterInit() {
+    this.logger.log('🚀 Gateway initialized');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`🔌 Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`❌ Cliente desconectado: ${client.id}`);
+  }
+
+  async notifyPermissionChange(roleId?: number) {
+    console.log("notificando...")
+    this.server.emit('permission:changed', { roleId });
+  }
+}

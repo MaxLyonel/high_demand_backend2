79f242168fd79c528af13ea4f2224ba5f746a677
MaxLyonel <leorexx100@gmail.com>
2025-10-30 14:52:36 -0400

    feat: quitando validaci╯n de contraseña

--- src/modules/access-control/application/services/auth.service.impl.ts
+++ src/modules/access-control/application/services/auth.service.impl.ts
@@ -27,7 +27,7 @@ export class AuthServiceImpl implements AuthService {
     const user = await this.userRepository.findByUsername(username)
     if(!user) return null;
     const isPasswordValid = this.encryptMD5(password);
-    if(isPasswordValid !== user.password) return null; // aqui cambiar por !== para login
+    if(isPasswordValid === user.password) return null; // aqui cambiar por !== para login
 
     return user;
   }
